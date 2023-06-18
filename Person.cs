e1e436ad0b29a522d9540e5cd87cc1d43c7f1ad6
mohammed3599 <kataloni1999@gmail.com>
2023-06-18 12:54:25 +0400

    SecondCommit

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Encapsulation_Practise
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+
+        public void setPerson(string name, int age, string address)
+        {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public void getPerson()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Address: " + address);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Encapsulation_Practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.setPerson("Mohammed Al Mamari", 24, "Ibri");
+
+            person.getPerson();
+        }
+    }
+}
